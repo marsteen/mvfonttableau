ca57491f9c4161c46ddbc13e1facab5805559cd6
Martin Steen <martin@martin-steen.de>
2021-02-05 17:45:24 +0100

    fonttableau fixed

--- project/create_tableaus_verdana.sh
+++ project/create_tableaus_verdana.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+./createfonttableau_opengl fonts/verdana.ttf 12 256
+./createfonttableau_opengl fonts/verdana.ttf 16 256
+./createfonttableau_opengl fonts/verdana.ttf 20 256
 ./createfonttableau_opengl fonts/verdana.ttf 24 512
-./createfonttableau_opengl fonts/verdana.ttf 48 1024
+./createfonttableau_opengl fonts/verdana.ttf 28 512
+./createfonttableau_opengl fonts/verdana.ttf 32 512
 
